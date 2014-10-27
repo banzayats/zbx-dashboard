c295c8e63391cac7b89a3f3e4a27f8a32e0d5e37
Вадим <neformat@gmail.com>
2014-10-27 16:17:15 +0200

    Update forms.py
    Fixed "E241 multiple spaces after ','"

--- zbx_dashboard/forms.py
+++ zbx_dashboard/forms.py
@@ -9,16 +9,16 @@ from django.utils.translation import ugettext as _
 
 class SelectForm(forms.Form):
     CHOICES = (
-        ('86400',    _("1 day")),
-        ('3600',     _("1 hour")),
-        ('7200',     _("2 hours")),
-        ('10800',    _("3 hours")),
-        ('21600',    _("6 hours")),
-        ('43200',    _("12 hours")),
-        ('604800',   _("1 week")),
-        ('1209600',  _("2 weeks")),
-        ('2592000',  _("1 month")),
-        ('7776000',  _("3 months")),
+        ('86400', _("1 day")),
+        ('3600', _("1 hour")),
+        ('7200', _("2 hours")),
+        ('10800', _("3 hours")),
+        ('21600', _("6 hours")),
+        ('43200', _("12 hours")),
+        ('604800', _("1 week")),
+        ('1209600', _("2 weeks")),
+        ('2592000', _("1 month")),
+        ('7776000', _("3 months")),
         ('15552000', _("6 months")),
         ('31536000', _("Year")),
     )
@@ -26,7 +26,7 @@ class SelectForm(forms.Form):
     select = forms.ChoiceField(
         widget=forms.Select(
             attrs={
-                'class': 'form-control  input-sm',
+                'class': 'form-control input-sm',
                 'onchange': 'this.form.submit();',
             }
         ),
